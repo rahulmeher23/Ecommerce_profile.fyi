819673838b3d4c97d892664fb0a67b82faeb6804
Rahul Meher <mikepmurphy100@gmail.com>
2024-08-19 14:50:04 +0530

    Update ProductDescription.jsx

--- src/pages/ProductDescription.jsx
+++ src/pages/ProductDescription.jsx
@@ -60,12 +60,12 @@ const ProductDescription = () => {
                         <div className='font-tenor flex flex-col flex-1 justify-between lg:p-10'>
                             <div className='h-[70%] md:h-[55%] lg:h-[50%] flex flex-col lg:grid lg:grid-cols-2 gap-5 lg:gap-10'>
                                 {/* Product Image */}
-                                <div className='h-[60vh] md:h-[50%] lg:h-[500px] flex justify-center items-center  md:rounded-lg'>
+                                <div className='h-[60vh] xl:h-[500px] flex justify-center items-center  md:rounded-lg'>
                                     {/* <div className='flex justify-center items-center h-full md:rounded-lg'> */}
                                     <img
                                         src={image}
                                         alt={name}
-                                        className='w-full  h-full lg:h-[80%] object-cover  xl:transform xl:transition-transform duration-300 xl:hover:scale-110 md:rounded-lg'
+                                        className='w-full  h-full xl:h-[80%] object-cover  xl:transform xl:transition-transform duration-300 xl:hover:scale-110 md:rounded-lg'
                                     />
                                     {/* </div> */}
                                 </div>
@@ -89,7 +89,7 @@ const ProductDescription = () => {
                                         </div>
 
                                         {/* add, remove from cart buttons in tablets and above  */}
-                                        <div className='hidden md:flex'>
+                                        {/* <div className='hidden xl:flex'>
                                             {
                                                 items.some((item) => item.id === product.id) ? (
                                                     <button onClick={() => handleRemovefromCartBtn(product?.id)} className='bg-black text-white z-20 cursor-pointer py-2 px-3 text-sm md:text-sm transform transition-transform duration-300 hover:scale-105'>
@@ -104,26 +104,26 @@ const ProductDescription = () => {
                                                     </button>
                                                 )
                                             }
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </div>
 
                             {/* add, remove from cart buttons in mobiles  */}
-                            <div className=' md:hidden w-full'>
+                            <div className=' flex self-center w-full xl:w-[80%]'>
                                 {
                                     items.some((item) => item.id === product.id) ? (
                                         <button onClick={() => handleRemovefromCartBtn(product?.id)} className='bg-black text-white z-20 cursor-pointer w-full  py-3 text-center text-xl md:text-sm transform transition-transform duration-300 hover:scale-105'>
                                             <div className='flex gap-5 items-center justify-center '>
-                                                <p className='text-white w-6 h-6'><ShopIcon /></p>
-                                                <p> REMOVE FROM CART</p>
+                                                <p className='text-white w-6 h-6    md:w-8 md:h-8   xl:w-10 xl:h-10  xk:w-8 2xl:h-8 text-center    '><ShopIcon /></p>
+                                                <p className='flex gap-5 items-center justify-center text-lg md:text-xl  xl:text-2xl'>REMOVE FROM CART</p>
                                             </div>
                                         </button>
                                     ) : (
                                         <button onClick={() => handleAddtoCartBtn(product)} className='bg-black text-white z-20 cursor-pointer w-full  py-3 text-center text-xl md:text-sm transform transition-transform duration-300 hover:scale-105'>
                                             <div className='flex gap-5 items-center justify-center '>
-                                                <p className='text-white w-6 h-6'><ShopIcon /></p>
-                                                <p>ADD TO CART</p>
+                                                <p className='text-white w-6 h-6    md:w-8 md:h-8   xl:w-10 xl:h-10  xk:w-8 2xl:h-8 text-center    '><ShopIcon /></p>
+                                                <p className='flex gap-5 items-center justify-center text-lg md:text-xl  xl:text-2xl'>ADD TO CART</p>
                                             </div>
                                         </button>
                                     )
